a7dec1ba7186f35cf59a2a7358b5dab725242af6
Rosa <rosa.segers.2001@gmail.com>
2024-01-15 10:59:37 +0100

    Added search for the BookingController

--- src/main/java/com/projectsem3/rooshapi/controllers/BookingController.java
+++ src/main/java/com/projectsem3/rooshapi/controllers/BookingController.java
@@ -4,11 +4,11 @@ import com.projectsem3.rooshapi.controllers.dtos.request.BookingRequest;
 import com.projectsem3.rooshapi.controllers.dtos.request.UpdateBookingRequest;
 import com.projectsem3.rooshapi.domain.Booking;
 import com.projectsem3.rooshapi.managers.BookingManager;
-import com.projectsem3.rooshapi.managers.OfficeManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin(maxAge = 3600)
 @RestController
@@ -19,5 +19,23 @@ public class BookingController extends GenericController<BookingRequest, Booking
 
     public BookingController(){
         super.classType = "Booking";
+    }
+
+    @GetMapping(value = "/search")
+    public List<Booking> bookingSearch(@RequestParam String referenceNumber, @RequestParam String vendor){
+        List<Booking> bookings = _manager.getItems();
+
+        if (!referenceNumber.isEmpty())
+            bookings = bookings
+                    .stream()
+                    .filter(b -> b.getReferenceNumber().toLowerCase().contains(referenceNumber.toLowerCase()))
+                    .collect(Collectors.toList());
+        if(!vendor.isEmpty())
+            bookings = bookings
+                    .stream()
+                    .filter(b -> b.getVendor().toLowerCase().contains(vendor.toLowerCase()))
+                    .collect(Collectors.toList());
+
+        return bookings;
     }
 }
